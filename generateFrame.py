047f2c96039d63c213154d5898cd592f3527bb1f
murch <ryan.c.murchie@gmail.com>
2017-01-09 21:32:21 -0500

    Add function skeleton

---
+++ generateFrame.py
@@ -0,0 +1,7 @@
+#Function for extracting individual frame from directory of TIFF files
+
+def generateFrame(filePath): #filePath - path of file directory containing TIFF files to load
+
+
+
+    return;
