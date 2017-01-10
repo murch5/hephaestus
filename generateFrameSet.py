cde4f71177e1eab7d57de437b8608975a4e6f78f
murch <ryan.c.murchie@gmail.com>
2017-01-10 02:05:36 -0500

    Comments, add func to FlattenTIFFdata

--- generateFrameSet.py
+++ generateFrameSet.py
@@ -8,21 +8,22 @@ import scipy as sp
 
 def FlattenTIFFData(TIFFdata): #TIFFdata - NumPy array of raw TIFF data
 
-
-    return;
+    flat = TIFFdata[:,:,2]
+    return flat;
 
 def generateFrameFromTIFF(filePath): #filePath - path of individual image file to load
 
     image = plt.imread(filePath)
 
-    return;
+    return image;
 
 def generateFrameSet(filePath): #filePath - path of file directory containing TIFF files to load
 
+    print ("TOP:    Loading script: generateFrameSet")
     print("LOG:    Start Frame Set Generation")
     frames = []
     fileList = glob.glob(filePath)
-    print(fileList)
+    print("LOG:       File list contains: " + str(len(fileList)) + " images")
     for file in fileList:
         print("LOG:    Load image Name: " + file)
         frames.append(generateFrameFromTIFF(file))
