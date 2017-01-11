5993ff740faacf38794701355784733ac91428be
murch <ryan.c.murchie@gmail.com>
2017-01-11 01:50:39 -0500

    load coordinate data for lines from tab-delim file add empty drawLines func

--- zb_peristalsis.py
+++ zb_peristalsis.py
@@ -1,21 +1,39 @@
 import contourMapAnim as cm
 import generateFrameSet as gf
+import numpy as np
 
 class zbPeristalsis():
 
-    def loadBoundaryCoord(fileName):
+    def drawLines(self):
+
+        return;
+    def loadBoundaryCoord(self,fileNames):
+
+        for file in fileNames:
+            print(file)
+            d = np.loadtxt(file)
+            print(d)
+            self.lineCoord.append(np.loadtxt(file,dtype=int,delimiter="\t"))
+
         return;
 
     def drawBoundaryCoord(self):
-        self.contourAnimationHandler.contourMapBase.assignLineDrawFunc("")
+        self.contourAnimationHandler.contourMapBase.assignLineDrawFunc(self.drawLines)
 
         return;
 
     def __init__(self):
         self.contourAnimationHandler = cm.contourMapAnim(gf.generateFrameSet("*.tif"))
-        self.contourAnimationHandler.startAnim()
+        self.lineCoord = []
 
+        fileNames = []
+
+        fileNames.append("Bottom.txt")
+        fileNames.append("Top.txt")
+        self.loadBoundaryCoord(fileNames)
         self.drawBoundaryCoord()
+
+        self.contourAnimationHandler.startAnim()
 
 
 
