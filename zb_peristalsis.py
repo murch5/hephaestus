456c6ae5bea0f6e10ff2db845fea1a8e94de232a
murch <ryan.c.murchie@gmail.com>
2017-01-11 20:58:41 -0500

    fixing creation of lineCollection by changing boundaryLines deref

--- zb_peristalsis.py
+++ zb_peristalsis.py
@@ -2,6 +2,7 @@ import contourMapAnim as cm
 import generateFrameSet as gf
 import numpy as np
 from matplotlib import lines as ln
+import matplotlib.collections as collect
 
 class zbPeristalsis():
 
@@ -9,7 +10,13 @@ class zbPeristalsis():
 
         for boundaryLines in self.lineCoord:
             print(boundaryLines)
-            self.contourAnimationHandler.subplot.add_line(ln.Line2D(boundaryLines[:,0],boundaryLines[:,1]))
+            print(type(boundaryLines))
+            print(boundaryLines.shape)
+            print(boundaryLines[:,0])
+           # lines = np.concatenate(boundaryLines[:,0],boundaryLines[:,1])
+            self.lineCollection = collect.LineCollection(boundaryLines[[]])
+            print(self.lineCollection)
+          #  self.contourAnimationHandler.subplot.add_line(ln.Line2D(boundaryLines[:,0],boundaryLines[:,1]))
         return;
 
     def initLines(self):
@@ -33,6 +40,7 @@ class zbPeristalsis():
     def __init__(self):
         self.contourAnimationHandler = cm.contourMapAnim(gf.generateFrameSet("*.tif"))
         self.lineCoord = []
+        self.lineCollection = None
 
         fileNames = []
 
