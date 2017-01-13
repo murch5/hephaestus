7ea24978f16f0c0140568c5de2300f2959151b01
murch <ryan.c.murchie@gmail.com>
2017-01-12 22:06:22 -0500

    maximize window size create flag for saving animation

--- plot_manager.py
+++ plot_manager.py
@@ -8,7 +8,7 @@ plt.rcParams['animation.ffmpeg_path'] = '/ffmpeg/bin/ffmpeg'
 class plot_manager():
 
     def __init__(self):
-        self.figure = plt.figure()
+        self.figure = plt.figure(figsize=(8, 4))
         self.plotList = []
         self.animHandler = None
         self.writer = None
@@ -30,13 +30,18 @@ class plot_manager():
 
 
     def startAnim(self):
+        mng = plt.get_current_fig_manager()
+        mng.window.state('zoomed')
+        self.saveAnimFlag = True
         self.animHandler = anim.FuncAnimation(self.figure, self.animatePlot, frames=self.framesPerImage * self.totalFrames, init_func=self.initAnim)
         if self.saveAnimFlag == True: self.saveAnimationToFile()
         plt.show()
         return;
 
     def saveAnimationToFile(self):
-        self.writer = anim.FFMpegWriter(fps=15)
+
+
+        self.writer = anim.FFMpegWriter(fps=15, bitrate=5000)
         self.animHandler.save("t.mp4",writer=self.writer)
         return;
 
