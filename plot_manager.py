69a8d60dd9436ecfb0812a62b2902976cebdc4a8
murch <ryan.c.murchie@gmail.com>
2017-01-12 20:59:53 -0500

    add func to save animation to FFMPEG .mp4

--- plot_manager.py
+++ plot_manager.py
@@ -3,19 +3,24 @@ from zb_peristalsis import zbPeristalsis
 from imageStack import imageStack
 import generateFrameSet as gf
 import matplotlib.animation as anim
+plt.rcParams['animation.ffmpeg_path'] = '/ffmpeg/bin/ffmpeg'
 
 class plot_manager():
 
     def __init__(self):
         self.figure = plt.figure()
         self.plotList = []
-        self.animHandler = []
+        self.animHandler = None
+        self.writer = None
         self.framesPerImage = 1
         self.totalFrames = 80
         self.currFrameIndex = 0
         self.currImageIndex = 0
+        self.saveAnimFlag = False
 
         return;
+
+
 
     def addPlot(self,plotClass,data, position, dataDir):
 
@@ -26,7 +31,13 @@ class plot_manager():
 
     def startAnim(self):
         self.animHandler = anim.FuncAnimation(self.figure, self.animatePlot, frames=self.framesPerImage * self.totalFrames, init_func=self.initAnim)
+        if self.saveAnimFlag == True: self.saveAnimationToFile()
         plt.show()
+        return;
+
+    def saveAnimationToFile(self):
+        self.writer = anim.FFMpegWriter(fps=15)
+        self.animHandler.save("t.mp4",writer=self.writer)
         return;
 
     def initAnim(self):
@@ -46,6 +57,8 @@ class plot_manager():
     def addLabel(self,text,position,colorMap,index):
         self.plotList[index].addTextAnnotation(text,position,colorMap)
         return;
+
+
 
 test1 = plot_manager()
 test1.addPlot(zbPeristalsis,gf.generateFrameSet("WT/*.tif"),121,"WT/")
