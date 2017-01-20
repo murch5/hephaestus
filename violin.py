c50a57df8c1a95f4cbb32cedb1fa4fb530beb061
murch <ryan.c.murchie@gmail.com>
2017-01-19 21:55:45 -0500

    base func of class

--- violin.py
+++ violin.py
@@ -2,4 +2,16 @@ import animPlot as animPlot
 
 class violin(animPlot.animPlot):
 
+    def __init__(self, figure, data, position):
+        animPlot.animPlot.__init__(self, figure, data, position)
+        return
+
+    def animate(self,i):
+        return;
+
+    def draw(self):
+        return;
+
+    def initAnimate(self, i):
+        return;
 
