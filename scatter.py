f1f3a741fad0809c8e7f86a6559868a3451881da
murch <ryan.c.murchie@gmail.com>
2017-01-22 13:54:57 -0500

    add init for inherited class - bug fix

--- scatter.py
+++ scatter.py
@@ -1,14 +1,15 @@
 import animPlot as animPlot
 import seaborn as sb
+import matplotlib as plt
 
 class scatter(animPlot.animPlot):
 
     def __init__(self,figure,data,position):
-
+        animPlot.animPlot.__init__(self,figure,data,position)
+        print(0)
 
     def draw(self):
-        sb.plt(sb.plt.scatter,x=self.data.iloc[:, 1],y=self.data.iloc[:, 2])
-
+        sb.regplot(x=self.data.iloc[:,0],y=self.data.iloc[:, 1],fit_reg=False,ax=self.subplot)
 
     def initAnimate(self):
         print(0)
