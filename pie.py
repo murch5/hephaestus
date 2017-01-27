61f19a7a01b938b995fab288a95f5e5a9a01e194
murch <ryan.c.murchie@gmail.com>
2017-01-26 21:34:03 -0500

    added automatic removed of NaN for pie charts

--- pie.py
+++ pie.py
@@ -1,10 +1,16 @@
 import animPlot as animPlot
+import numpy as np
 
 class pie(animPlot.animPlot):
 
     def __init__(self, figure, data, position, title="", explode=0):
         animPlot.animPlot.__init__(self, figure, data, position,title)
         self.explode = explode
+        self.NaNindices = self.data.isnull()
+        self.numNan = np.sum(self.NaNindices)
+        self.dataCleaned = self.data[self.data.notnull()]
+
+        print(self.dataCleaned)
         return
 
     def animate(self,i):
@@ -16,9 +22,9 @@ class pie(animPlot.animPlot):
             return ('%.2f' % pct) if pct > 1 else ''
 
         if self.explode != 0:
-            self.subplot.pie(self.data,labels=list(self.data.index), startangle=90, explode=self.explode, autopct=my_autopct)
+            self.subplot.pie(self.dataCleaned,labels=list(self.dataCleaned.index), startangle=90, explode=self.explode, autopct=my_autopct)
         else:
-            self.subplot.pie(self.data, labels=list(self.data.index), startangle=90,autopct=my_autopct)
+            self.subplot.pie(self.dataCleaned, labels=list(self.dataCleaned.index), startangle=90,autopct=my_autopct)
 
         self.subplot.axis("equal")
 
