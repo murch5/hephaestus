9bdbbdea7ac583c272f32df5dd24b4cb3f77f6a4
murch <ryan.c.murchie@gmail.com>
2017-02-01 20:22:06 -0500

    move plotting to draw func

--- forest.py
+++ forest.py
@@ -9,10 +9,24 @@ class forest(animPlot.animPlot):
     def __init__(self, figure, data, position,title=""):
         animPlot.animPlot.__init__(self, figure, data, position,title)
 
-        self.initializeForest()
+        #self.initializeForest()
 
         return
     def draw(self):
+
+        Y = np.arange(len(self.data.index))
+        print(self.data)
+        self.subplot.plot(self.data.ix[:, 1], Y, "rh")
+
+        for index, row in self.data.iterrows():
+            yCoord = [Y[index], Y[index]]
+            self.subplot.plot(row[2:4], yCoord, "r-")
+
+        self.subplot.set_yticks(Y)
+        self.subplot.grid(axis="y")
+        self.subplot.set_yticklabels(self.data.ix[:, 0])
+        self.subplot.set_xlabel("Odds Ratio")
+
         return
     def initAnimate(self):
         print(0)
@@ -28,11 +42,6 @@ class forest(animPlot.animPlot):
         self.subplot.plot(self.data.ix[:,1],Y,"rh")
 
         for index,row in self.data.iterrows():
-          #  self.subplot.plot(dataRo)
-
-            print(row)
-            print(row[2:4])
-            print(Y[index])
             yCoord = [Y[index],Y[index]]
             self.subplot.plot(row[2:4], yCoord, "r-")
 
@@ -41,6 +50,5 @@ class forest(animPlot.animPlot):
         self.subplot.set_yticklabels(self.data.ix[:,0])
         self.subplot.set_xlabel("Odds Ratio")
 
-        #print(Y)
         return
 
