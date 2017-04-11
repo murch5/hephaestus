5e43b29dd0003d544c15c1a3c94ad88d442ca0e2
murch <ryan.c.murchie@gmail.com>
2017-04-11 10:26:43 -0400

    add inherited class for variantTrack (from track)

---
+++ variantTrack.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+#import hgvs.parser as hgvsparser
+import track as track
+
+class variantTrack(track.track):
+
+    def __init__(self,figure, data, position,title="",plotArgs=[],annotate=[]):
+        track.track.__init__(self, figure, data, position, title, plotArgs, annotate)
+        self.variants = self.data.getFeature("variant")
+
+        self.featureLabel = "Note"
+
+    def draw(self):
+
+        self.trackAnnotations()
+
+        self.subplot.set_ylim([0.5, 1.5])
+        self.subplot.title.set_text("")
+        self.subplot.axis("off")
+
+
+        return;
+
+    def parseHGVS(self):
+        return
