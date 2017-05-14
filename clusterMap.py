f4cd6dd8a11a54c3698a57863c900aa1f38b88f2
murch <ryan.c.murchie@gmail.com>
2017-05-14 19:44:01 -0400

    add plotting settings for clusterMap

--- clusterMap.py
+++ clusterMap.py
@@ -13,15 +13,25 @@ class clusterMap(plot.plot):
 
     def draw(self):
 
-        self.clusterMap = sb.clustermap(self.data,method="average",row_cluster=True,vmin=0,vmax=300, figsize=(25,12))
+        if self.retrieveArgVal("vmin") is not None:
+            self.vmin = int(self.retrieveArgVal("vmin"))
+        if self.retrieveArgVal("vmax") is not None:
+            self.vmax = int(self.retrieveArgVal("vmax"))
+
+        print(self.vmin)
+        print(self.vmax)
+        self.clusterMap = sb.clustermap(self.data,method="average",row_cluster=True,vmin=self.vmin,vmax=self.vmax, figsize=(25,12))
 
         plt.setp(self.clusterMap.ax_heatmap.get_yticklabels(), rotation=0)
         plt.setp(self.clusterMap.ax_heatmap.get_xticklabels(), rotation=90)
 
-        self.clusterMap.ax_heatmap.xaxis.set_visible(False)
-        self.clusterMap.ax_heatmap.yaxis.set_visible(False)
+        if self.retrieveArgVal("hideXlabel") is not None:
+            self.clusterMap.ax_heatmap.xaxis.set_visible(False)
+        if self.retrieveArgVal("hideYlabel") is not None:
+            self.clusterMap.ax_heatmap.yaxis.set_visible(False)
 
-        self.clusterMap.savefig("test1.png")
+        #self.clusterMap.savefig("test1.png")
+
         return;
 
     def __init__(self,figure, data, position,title="",plotArgs=[],annotate=[]):
@@ -29,6 +39,8 @@ class clusterMap(plot.plot):
         self.position = position
         self.fig = figure
         self.clusterMap = 0
+        self.vmin = 0
+        self.vmax = 100
 
         self.data.set_index("GeneName",inplace=True)
 
