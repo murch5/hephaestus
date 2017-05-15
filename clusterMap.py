09753735db00678cdeb237d08f08d4da3c2d65a8
murch <ryan.c.murchie@gmail.com>
2017-05-15 17:02:19 -0400

    add func to save or retrieve precalc hierarchy

--- clusterMap.py
+++ clusterMap.py
@@ -1,7 +1,14 @@
 import plot as plot
 import seaborn as sb
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
+
+
+
+import scipy.spatial.distance as sp_spatial_distance
+import scipy.cluster.hierarchy as sp_cluster_hierarchy
 
 import sys
 sys.setrecursionlimit(40000)
@@ -18,9 +25,19 @@ class clusterMap(plot.plot):
         if self.retrieveArgVal("vmax") is not None:
             self.vmax = int(self.retrieveArgVal("vmax"))
 
-        print(self.vmin)
-        print(self.vmax)
-        self.clusterMap = sb.clustermap(self.data,method="average",row_cluster=True,vmin=self.vmin,vmax=self.vmax, figsize=(25,12))
+        if self.retrieveArgVal("SaveHierarchyClust") is not None:
+            data = self.data.T
+            pairwiseDist = sp_spatial_distance.pdist(data)
+            self.clusterHierarchy = sp_cluster_hierarchy.linkage(pairwiseDist,method="complete")
+            self.clusterHierarchy.dump("process_data/hierarchy/cluster/"+ str(self.retrieveArgVal("SaveHierarchyClust")) + ".cluster")
+
+        if self.retrieveArgVal("GetHierarchyClust") is not None:
+            self.clusterHierarchy = np.load(
+                "process_data/hierarchy/cluster/" + str(self.retrieveArgVal("GetHierarchyClust")) + ".cluster")
+
+        print(self.clusterHierarchy.shape)
+
+        self.clusterMap = sb.clustermap(self.data,col_linkage=self.clusterHierarchy,vmin=self.vmin,vmax=self.vmax, figsize=(25,12))
 
         plt.setp(self.clusterMap.ax_heatmap.get_yticklabels(), rotation=0)
         plt.setp(self.clusterMap.ax_heatmap.get_xticklabels(), rotation=90)
@@ -30,7 +47,7 @@ class clusterMap(plot.plot):
         if self.retrieveArgVal("hideYlabel") is not None:
             self.clusterMap.ax_heatmap.yaxis.set_visible(False)
 
-        #self.clusterMap.savefig("test1.png")
+        self.clusterMap.savefig(self.plotTitle + ".png")
 
         return;
 
@@ -41,6 +58,7 @@ class clusterMap(plot.plot):
         self.clusterMap = 0
         self.vmin = 0
         self.vmax = 100
+        self.clusterHierarchy = 0
 
         self.data.set_index("GeneName",inplace=True)
 
