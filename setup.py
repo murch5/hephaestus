e27b918535ab24d35fbade8764ba01956875b23e
murch <ryan.c.murchie@gmail.com>
2017-05-18 15:38:56 -0400

    add setup.py for install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='plot',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='None',
+    author='Ryan',
+    author_email='',
+    description='Plot Manager Wrapper for Matplotlib/seaborn'
+)
