13283909ffb5ccfa5d327a0cd205cb162133c73a
murch <ryan.c.murchie@gmail.com>
2017-05-22 00:03:00 -0400

    updated venn plotclass to use datatypes classes for data updated class name and init args

--- plotmanager/plottype/venn.py
+++ plotmanager/plottype/venn.py
@@ -4,32 +4,31 @@ import matplotlib_venn as plotvenn
 from plotmanager.plottype import plot
 
 
-class venn(plot.plot):
+class Venn(plot.Plot):
 
     def animate(self,i):
-        return;
+        return
 
     def draw(self):
 
         if self.groups == 2:
-            set1 = set(self.data.ix[:,0])
-            set2 = set(self.data.ix[:,1])
-            setNames = self.data.columns.values.tolist()
+            set1 = set(self.data.get().ix[:,0])
+            set2 = set(self.data.get().ix[:,1])
+            setNames = self.data.get().columns.values.tolist()
             self.venn = plotvenn.venn2([set1,set2],ax=self.subplot, set_labels=setNames)
         elif self.groups == 3:
-            set1 = set(self.data.ix[:, 0])
-            set2 = set(self.data.ix[:, 1])
-            set3 = set(self.data.ix[:, 1])
-            setNames = self.data.columns.values.tolist()
+            set1 = set(self.data.get().ix[:, 0])
+            set2 = set(self.data.get().ix[:, 1])
+            set3 = set(self.data.get().ix[:, 1])
+            setNames = self.data.get().columns.values.tolist()
             self.venn = plotvenn.venn3([set1, set2,set3],ax=self.subplot, set_labels=setNames)
 
         return;
 
-    def __init__(self,figure, data, position,title="",plotArgs=[],annotate=[]):
-        plot.plot.__init__(self,figure, data, position,title,plotArgs,annotate)
-        self.position = position
+    def __init__(self,figure, data, plot_XML):
+        plot.Plot.__init__(self,figure, data, plot_XML)
         self.fig = figure
-        self.groups = len(self.data.columns)
+        self.groups = len(self.data.get().columns)
         self.venn = 0
 
     def initAnimate(self, i):
