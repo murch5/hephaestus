8fe9b438a7b57468ab6f031b5c782bfbdcd5675b
murch <ryan.c.murchie@gmail.com>
2017-05-23 20:01:37 -0400

    update func for XML access; add z-score opt; clean up

--- plotmanager/plottype/clusterMap.py
+++ plotmanager/plottype/clusterMap.py
@@ -1,74 +1,77 @@
 from plotmanager.plottype import plot
 import seaborn as sb
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
 import glob as glob
 import os as os
-
-
-
 
 import scipy.spatial.distance as sp_spatial_distance
 import scipy.cluster.hierarchy as sp_cluster_hierarchy
 
 import sys
+
 sys.setrecursionlimit(40000)
 
 class ClusterMap(plot.Plot):
-
-    def animate(self,i):
-        return;
+    def animate(self, i):
+        return
 
     def draw(self):
 
-        if self.retrieveArgVal("vmin") is not None:
-            self.vmin = int(self.retrieveArgVal("vmin"))
-        if self.retrieveArgVal("vmax") is not None:
-            self.vmax = int(self.retrieveArgVal("vmax"))
+        if self.checkXML(".//plot_style//vmin"):
+            self.vmin = self.getXMLvalue(".//plot_style//vmin")
+        if self.checkXML(".//plot_style//vmax"):
+            self.vmax = self.getXMLvalue(".//plot_style//vmax")
 
-        if self.retrieveArgVal("SaveHierarchyClust") is not None:
-            data = self.data.T
-            pairwiseDist = sp_spatial_distance.pdist(data)
-            self.clusterHierarchy = sp_cluster_hierarchy.linkage(pairwiseDist,method="complete")
-            clusterNameOutput = str(self.retrieveArgVal("SaveHierarchyClust")) + ".cluster"
-            clusterOutputPath = os.path.join("./process/hierarchy/cluster/",clusterNameOutput)
+        if self.checkXML(".//plot_style//save_hierarchy_clust"):
+            data_transpose = self.data.get().T
+            pairwise_dist = sp_spatial_distance.pdist(data_transpose)
+            self.clusterHierarchy = sp_cluster_hierarchy.linkage(pairwise_dist, method="complete")
+            clusterNameOutput = str(self.plot_XML.find(".//plot_style//save_hierarchy_clust").text) + ".cluster"
+            clusterOutputPath = os.path.join("./process/hierarchy/cluster/", clusterNameOutput)
             self.clusterHierarchy.dump(clusterOutputPath)
 
-        if self.retrieveArgVal("GetHierarchyClust") is not None:
-            clusterNameInput = str(self.retrieveArgVal("GetHierarchyClust")) + ".cluster"
-            clusterInputPath = os.path.join("./process/hierarchy/cluster/", clusterNameInput)
-            self.clusterHierarchy = np.load(clusterInputPath)
+        if self.checkXML(".//plot_style//get_hierarchy_clust"):
+            clusterinput_name = str(self.plot_XML.find(".//plot_style//get_hierarchy_clust").text) + ".cluster"
+            clusterinput_path = os.path.join("./process/hierarchy/cluster/", clusterinput_name)
+            self.clusterHierarchy = np.load(clusterinput_path)
+
+        if self.checkXML(".//plot_style//std_scale"):
+            self.standard_scale = self.getXMLvalue(".//plot_style//std_scale")
+
+        if self.checkXML(".//plot_style//z_score"):
+            self.z_score = self.getXMLvalue(".//plot_style//z_score")
+
+        if self.checkXML(".//plot_style//fig_size"):
+            self.z_score = self.getXMLvalue(".//plot_style//fig_size")
 
 
+        self.cluster_map = sb.clustermap(self.data[0].get(), col_linkage=self.clusterHierarchy, vmin=self.vmin,
+                                         vmax=self.vmax, standard_scale=self.standard_scale, z_score=self.z_score,
+                                         figsize=(25, 12))
 
-        self.clusterMap = sb.clustermap(self.data,col_linkage=self.clusterHierarchy,vmin=self.vmin,vmax=self.vmax, figsize=(25,12))
+        plt.setp(self.cluster_map.ax_heatmap.get_yticklabels(), rotation=0)
+        plt.setp(self.cluster_map.ax_heatmap.get_xticklabels(), rotation=90)
 
-        plt.setp(self.clusterMap.ax_heatmap.get_yticklabels(), rotation=0)
-        plt.setp(self.clusterMap.ax_heatmap.get_xticklabels(), rotation=90)
+        if self.checkXML(".//plot_style//hide_xlabel"):
+            self.cluster_map.ax_heatmap.xaxis.set_visible(False)
+        if self.checkXML(".//plot_style//hide_ylabel"):
+            self.cluster_map.ax_heatmap.yaxis.set_visible(False)
 
-        if self.retrieveArgVal("hideXlabel") is not None:
-            self.clusterMap.ax_heatmap.xaxis.set_visible(False)
-        if self.retrieveArgVal("hideYlabel") is not None:
-            self.clusterMap.ax_heatmap.yaxis.set_visible(False)
+        return
 
-        self.clusterMap.savefig("./output/"+ self.plotTitle + ".png")
-
-        return;
-
-    def __init__(self,figure, data, plot_XML):
-        plot.Plot.__init__(self,figure, data, plot_XML)
-        self.position = position
-        self.fig = figure
-        self.clusterMap = 0
+    def __init__(self, figure, data, plot_XML):
+        plot.Plot.__init__(self, figure, data, plot_XML)
+        self.cluster_map = 0
         self.vmin = 0
         self.vmax = 100
-        self.clusterHierarchy = 0
-        os.makedirs("./process/hierarchy/cluster/",exist_ok=True)
+        self.clusterHierarchy = None
+        self.standard_scale = None
+        self.z_score = None
+        os.makedirs("./process/hierarchy/cluster/", exist_ok=True)
         self.outputHierarchy = glob.glob("./process/hierarchy/cluster/")
 
-        self.data.set_index("GeneName",inplace=True)
-
+        self.data[0].get().set_index("GeneName", inplace=True)
 
     def initAnimate(self, i):
-        return;
+        return
