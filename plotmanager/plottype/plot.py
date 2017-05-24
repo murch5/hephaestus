39bcb4094f205b027d7a9b890e7225bd4f321fcc
murch <ryan.c.murchie@gmail.com>
2017-05-23 20:02:10 -0400

    update func to access XML; add opt to load multiple dataset clean up

--- plotmanager/plottype/plot.py
+++ plotmanager/plottype/plot.py
@@ -9,6 +9,12 @@ class Plot():
     def __init__(self,figure, data, plot_XML):
         self.figure = figure
         self.data = data
+
+        #create data name dict for reference
+
+        self.data_index_dict = {}
+        self.set_data_dict()
+
         self.gridspec = None
         self.subplot = None
 
@@ -16,21 +22,26 @@ class Plot():
 
         return
 
+    def set_data_dict(self):
+
+        for i,datum in enumerate(self.data):
+            self.data_index_dict.update({datum.get_name(): i})
+
     def set_gridspec(self,gridspec):
         self.gridspec = gridspec
 
     def animate(self,i):
 
-        return;
+        return
 
     def initAnimate(self, i):
-        return;
+        return
 
     def draw(self):
-        return;
+        return
 
     def init(self):
-        return;
+        return
 
     def setup_subplot(self):
 
@@ -41,9 +52,38 @@ class Plot():
 
         subplotspec = self.gridspec.new_subplotspec([row,col], row_width, col_width)
 
-        self.subplot = self.figure.add_subplot(subplotspec)
+        if self.figure is not None:
+            self.subplot = self.figure.add_subplot(subplotspec)
 
         return
+
+    def checkXML(self,xml):
+        check = False
+        if self.plot_XML.find(xml) is not None:
+            check = True
+        else:
+            check = False
+
+        return check
+
+    def getXMLvalue(self,xml):
+        data = self.plot_XML.find(xml)
+        data_type = data.attrib["data_type"]
+        value = None
+        if data_type in ["int","i"]:
+            value = int(data.text)
+        elif data_type in ["float","f"]:
+            value = float(data.text)
+        elif data_type in ["bool","b"]:
+            value = bool(data.text)
+        elif data_type in ["str","s"]:
+            value = str(data.text)
+        elif data_type in ["tuple_int", "ti"]:
+            value = tuple(data.text.split(","))
+        else:
+            value = str(data.text)
+
+        return value
 
 
 
