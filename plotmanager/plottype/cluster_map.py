e2d8aaa95f0c77839d8cc44bd024e818048e2a7d
murch <ryan.c.murchie@gmail.com>
2017-06-04 19:47:24 -0400

    moved groupby func to be init_func add logging fixed col hierarchy

--- plotmanager/plottype/cluster_map.py
+++ plotmanager/plottype/cluster_map.py
@@ -3,6 +3,10 @@ import seaborn as sb
 import matplotlib.pyplot as plt
 import pandas as pd
 
+import logging as logging
+logger = logging.getLogger(__name__)
+
+
 import numpy as np
 import glob as glob
 import os as os
@@ -20,11 +24,17 @@ class ClusterMap(plot.Plot):
 
     def hierarchical_clustering(self, settings):
 
+        logger.debug("------ Hierarchical clustering enabled")
+
         if settings.get("source") == "save":
+
             data_transpose = self.data.get().T
             pairwise_dist = sp_spatial_distance.pdist(data_transpose)
             self.clusterHierarchy = sp_cluster_hierarchy.linkage(pairwise_dist, method="complete")
             clusterNameOutput = str(settings.get("name")) + ".cluster"
+
+            logger.debug("--------- Save hierarchy as:" + str(clusterNameOutput))
+
             clusterOutputPath = os.path.join("./process/hierarchy/cluster/", clusterNameOutput)
             self.clusterHierarchy.dump(clusterOutputPath)
 
@@ -36,18 +46,18 @@ class ClusterMap(plot.Plot):
 
         return
 
-    def draw(self):
+    def group_by(self, settings):
 
-        if self.getXMLsubset(".//plot_style//groupby_color") is not None:
+        if self.init_func.get("groupby_color") is not None:
 
             subset = self.getXMLsubset(".//plot_style//groupby_color")
 
             for set in subset.findall(".//grouping"):
-                axis = self.getXMLvalue(".//axis",set)
+                axis = self.getXMLvalue(".//axis", set)
 
-                groups_color_mapping = self.getXMLvalue(".//colormap_name",set)
+                groups_color_mapping = self.getXMLvalue(".//colormap_name", set)
 
-                mapping_subset = self.getXMLsubset(".//name_mapping",set)
+                mapping_subset = self.getXMLsubset(".//name_mapping", set)
 
                 name_transforms = mapping_subset.findall(".//mapping")
 
@@ -56,46 +66,45 @@ class ClusterMap(plot.Plot):
                 groups_name = index_names
 
                 for maps in name_transforms:
+                    dataset_map = self.getXMLvalue(".//name", maps)
 
-                    dataset_map = self.getXMLvalue(".//name",maps)
+                    groups_name = self.data.map_transform(groups_name, self.data.get_byname(dataset_map))
 
-                    groups_name = self.data.map_transform(groups_name,self.data.get_byname(dataset_map))
-
-
-
-
-                if self.getXMLvalue(".//color_map",set):
-                    group_color_palette = sb.color_palette(self.getXMLvalue(".//color_map",set),len(groups_name))
-                    group_color_pal_dict = dict(zip(groups_name,group_color_palette))
+                if self.getXMLvalue(".//color_map", set):
+                    group_color_palette = sb.color_palette(self.getXMLvalue(".//color_map", set), len(groups_name))
+                    group_color_pal_dict = dict(zip(groups_name, group_color_palette))
 
                     ### Override nan to white
-                    group_color_pal_dict[np.nan] = (1.0,1.0,1.0)
+                    group_color_pal_dict[np.nan] = (1.0, 1.0, 1.0)
 
                     group_color = self.data.map_transform(groups_name, group_color_pal_dict)
                 else:
-                    group_color = self.data.map_transform(groups_name,self.data.get_byname(groups_color_mapping))
+                    group_color = self.data.map_transform(groups_name, self.data.get_byname(groups_color_mapping))
 
+                groups_indexed = pd.concat([index_names, group_color], ignore_index=False, axis=1)
+                groups_indexed.columns = ["Indices", "Classification", ]
+                groups_indexed.set_index("Indices", inplace=True)
 
-                groups_indexed = pd.concat([index_names,group_color],ignore_index=False, axis=1)
-                groups_indexed.columns  = ["Indices","Classification",]
-                groups_indexed.set_index("Indices",inplace=True)
-
-                if axis==1:
+                if axis == 1:
                     self.col_color_groups = groups_indexed
-                elif axis==0:
+                elif axis == 0:
                     self.row_color_groups = groups_indexed
 
-        self.cluster_map = sb.clustermap(self.data.get(), col_linkage=None, vmin=self.get_set_param("vmin"), row_linkage=None,
+        return
+
+    def draw(self):
+
+        self.cluster_map = sb.clustermap(self.data.get(), col_linkage=self.clusterHierarchy, vmin=self.get_set_param("vmin"), row_linkage=None,
                                          vmax=self.get_set_param("vmax"), standard_scale=self.get_set_param("std_scale"), z_score=self.get_set_param("z_score"),col_colors=self.col_color_groups,
                                          row_colors = self.row_color_groups, figsize=(15, 8))
 
         plt.setp(self.cluster_map.ax_heatmap.get_yticklabels(), rotation=0)
         plt.setp(self.cluster_map.ax_heatmap.get_xticklabels(), rotation=90)
 
-        if self.checkXML(".//plot_style//hide_xlabel"):
-            self.cluster_map.ax_heatmap.xaxis.set_visible(False)
-        if self.checkXML(".//plot_style//hide_ylabel"):
-            self.cluster_map.ax_heatmap.yaxis.set_visible(False)
+        #if self.checkXML(".//plot_style//hide_xlabel"):
+       #     self.cluster_map.ax_heatmap.xaxis.set_visible(False)
+       # if self.checkXML(".//plot_style//hide_ylabel"):
+        #    self.cluster_map.ax_heatmap.yaxis.set_visible(False)
 
         return
 
@@ -116,7 +125,7 @@ class ClusterMap(plot.Plot):
 
         self.data.get().set_index("GeneName", inplace=True)
 
-        self.init_func_dict = {"hierarchy": self.hierarchical_clustering}
+        self.init_func_list = {"hierarchy": self.hierarchical_clustering}
 
         return
 
