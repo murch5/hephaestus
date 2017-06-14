c0b9c9c5590f5b484154300a7c0fdacade1e5ac5
murch <ryan.c.murchie@gmail.com>
2017-06-13 22:54:33 -0400

    migrate to factory-manager system (removed __init__, remove old funcs) rename Violin yml to class name for ref

--- plotmanager/plottype/violin.py
+++ plotmanager/plottype/violin.py
@@ -2,28 +2,14 @@ import seaborn as sb
 
 from plotmanager.plottype import anim_plot
 
-
 class Violin(anim_plot.AnimPlot):
-
-    def __init__(self,figure, data, plot_settings):
-        anim_plot.AnimPlot.__init__(self, figure, data, plot_settings)
-        self.type = "violin"
-        self.violinPlot = 0
-        return
-
-    def animate(self,i):
-        return
 
     def draw(self):
 
-        if(len(self.data.get().columns)==3):
-            self.violinPlot = sb.violinplot(x=self.data.get().iloc[:, 0], y=self.data.get().iloc[:, 1],hue=self.data.get().iloc[:, 2],split=True,ax=self.subplot)
+        if(len(self.data[0].columns)==3):
+            self.violinPlot = sb.violinplot(x=self.data[0].iloc[:, 0], y=self.data[0].iloc[:, 1],hue=self.data[0].iloc[:, 2],split=True,ax=self.subplot)
         else:
-            self.violinPlot = sb.violinplot(x=self.data.get().iloc[:,0],y=self.data.get().iloc[:,1],ax=self.subplot)
+            self.violinPlot = sb.violinplot(x=self.data[0].iloc[:,0],y=self.data[0].iloc[:,1],ax=self.subplot)
 
-
-        return
-
-    def initAnimate(self, i):
         return
 
