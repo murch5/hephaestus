f061a9c001a4437e32474f088bdd0cb163927a3e
murch <ryan.c.murchie@gmail.com>
2017-06-13 22:54:07 -0400

    migrate to factory-manager system (removed __init__, remove old funcs) rename Pie yml to class name for ref

--- plotmanager/plottype/pie.py
+++ plotmanager/plottype/pie.py
@@ -6,19 +6,14 @@ import matplotlib.pyplot as plt
 
 class Pie(anim_plot.AnimPlot):
 
-    def __init__(self,figure, data, plot_settings):
-        anim_plot.AnimPlot.__init__(self, figure, data, plot_settings)
-        self.explode = None
-        self.labels = pd.DataFrame(self.data.get().index.tolist())
-        self.labels.columns = ["Labels"]
-
-        self.pct_filter = 1
-        self.type = "pie"
-        self.colors = ["#6a6dc5","#76b74b","#a656c4","#68803b","#c666a8","#4eb596","#ca5938","#689bd6","#c49643","#ca586f"]
-
-        return
-
     def draw(self):
+
+        print(self.colors)
+        self.explode = None
+
+        self.labels = pd.DataFrame(self.data[0].index.tolist())
+        print(self.labels)
+        self.labels.columns = ["Labels"]
 
         def make_autopct(values):
             def my_autopct(pct):
@@ -33,8 +28,8 @@ class Pie(anim_plot.AnimPlot):
 
                 val=int((pct*total/100.0)+0.5)
 
-                if self.get_set_param("show_pct")==True:
-                    wedgeLabel = '{p:.2f}%  ({v:d})'.format(p=pct, v=val) if pct > self.get_set_param("pct_filter") else ''
+                if self.show_pct:
+                    wedgeLabel = '{p:.2f}%  ({v:d})'.format(p=pct, v=val) if pct > self.pct_filter else ''
                 else:
                     wedgeLabel = '({v:d})'.format(v=val) if pct > self.pct_filter else ''
 
@@ -43,21 +38,22 @@ class Pie(anim_plot.AnimPlot):
             return my_autopct
 
 
-        if self.checkXML(".//plot_style//hide_labels_byvalue"):
-            threshold = self.getXMLvalue(".//plot_style//hide_labels_byvalue")
-            hiddenVal = self.data.get().to_frame(name="val").query("val"+ threshold)
+        if self.get("hide_label_by_value"):
+
+            threshold = self.get("hide_label_by_value")
+            hiddenVal = self.data[0].to_frame(name="val").query("val"+ threshold)
             print(hiddenVal)
             labelsToKeep = hiddenVal.index.tolist()
             print(labelsToKeep)
             if labelsToKeep is not None:
                 self.labels.replace(labelsToKeep," ",inplace=True)
 
-        if len(self.data.get()) % 2 == 0:
-            colorWedge = ["#9F4298", "#D1AFD3"]
+        if len(self.data[0]) % 2 == 0:
+            color_wedge = ["#9F4298", "#D1AFD3"]
         else:
-            colorWedge = ["#9F4298", "#D1AFD3","#E6E7E8"]
+            color_wedge = ["#9F4298", "#D1AFD3","#E6E7E8"]
 
-        self.subplot.pie(self.data.get(),labels=self.labels.ix[:,0], startangle=90, explode=self.explode, autopct=make_autopct(self.data.get()),colors=colorWedge)
+        self.subplot.pie(self.data[0],labels=self.labels.ix[:,0], startangle=90, explode=self.explode, autopct=make_autopct(self.data[0]),colors=color_wedge)
 
         self.subplot.axis("equal")
 
