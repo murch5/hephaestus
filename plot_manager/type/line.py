344bc55fc3c55e379fcac7adac0cd32047306140
murch <ryan.c.murchie@gmail.com>
2017-08-16 10:01:52 -0400

    remove old data shape check for Line class

--- plot_manager/type/line.py
+++ plot_manager/type/line.py
@@ -9,13 +9,6 @@ class Line(anim_plot.AnimPlot):
 
     def draw(self):
 
-
-      #  if isinstance(self.data[0],np.ndarray):
-          #  self.data[0] = pd.DataFrame(self.data[0])
-           # print("test")
-        #print(self.data[0].shape)
-
-        print(len(self.data[0].shape))
         if len(self.data[0].shape) == 1:
             self.subplot.plot(self.data[0], "-")
         elif len(self.data[0].shape) == 2:
