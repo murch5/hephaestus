40eb6423969337785f198d09a737e4eb20bc2be2
murch <ryan.c.murchie@gmail.com>
2017-09-13 12:12:15 -0400

    update draw func in scatter using sb.lmplot

--- plot_manager/type/scatter.py
+++ plot_manager/type/scatter.py
@@ -7,6 +7,14 @@ class Scatter(anim_plot.AnimPlot):
 
     def draw(self):
 
-        sb.regplot(x=self.data[0].ix[:,0],y=self.data[0].ix[:, 1],fit_reg=True,ax=self.subplot)
+        #self.subplot = sb.regplot(x=self.data[0].ix[:,0],y=self.data[0].ix[:, 1],fit_reg=False,ax=self.subplot, scatter_kws={"color":self.data[0].ix[:, 1]})
+
+        #scatter = self.subplot.scatter(x=self.data[0].ix[:,0],y=self.data[0].ix[:, 1], c=self.data[0].ix[:, 2])
+
+
+        lmplot = sb.lmplot(x=self.data[0].ix[:,0].name,y=self.data[0].ix[:, 1].name,data=self.data[0],fit_reg=False, hue=self.data[0].ix[:,2].name, legend_out=True)
+
+        lmplot.fig.set_size_inches(12,12)
+        self.figure = lmplot.fig
 
         pass
